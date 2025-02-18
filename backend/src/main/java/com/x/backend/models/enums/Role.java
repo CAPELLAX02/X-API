1508380586a6616fcb9283c67c0d2e56c87aae3a
CAPELLAX02 <ahmtatar@gmail.com>
2025-02-18 17:10:55 +0300

    Create the application user and role entities - Repository and service layer starter

---
+++ backend/src/main/java/com/x/backend/models/enums/Role.java
@@ -0,0 +1,45 @@
+package com.x.backend.models.enums;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer roleId;
+
+    private String authority;
+
+    public Role() {}
+
+    public Role(Integer roleId, String authority) {
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "roleId=" + roleId +
+                ", authority='" + authority + '\'' +
+                '}';
+    }
+}
